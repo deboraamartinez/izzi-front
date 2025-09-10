42980e10f9ebe66be93781f6ff7ffaf13779e183
debora.martinez <deboraaspesimartinez@gmail.com>
2025-09-10 22:58:18 +0100

    fix: remove assetPrefix for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,14 @@
-// next.config.mjs
-
 const isProd = process.env.NODE_ENV === "production";
-const repo = "izzi-front"; // 👈 troca pelo nome do seu repositório
+const repo = "izzi-front";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 
-  // 👇 acrescenta isso:
   output: "export",
   basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}` : "",
   env: {
     NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : "",
   },
